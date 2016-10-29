a3ceb728ed273d1c92f8c2752a1fce88fdd104c8
Jemart Pacilan <jemartpacilan@gmail.com>
2016-10-29 21:36:26 +0800

    week2
    week2(initGame)

--- src/Game.hs
+++ src/Game.hs
@@ -5,8 +5,8 @@ import Common
 initGame :: Int -> State
 
 -- TODO: Implement a method to initialize a new game given n players
-initGame n = State { players = [ ],
-                     deck = fullDeck,
+initGame n = State { players =  [HPlayer { name = "Player" ++ show xs, hand =  []} | xs <- [1..n] ],
+                     deck = Common.fullDeck,
                      d_stack = [ ] }
 
 -- TODO: Implement a method to setup the game
--- src/Shuffler.hs
+++ src/Shuffler.hs
@@ -2,7 +2,9 @@ module Shuffler where
 
 import Common
 
+
 shuffleDeck :: State -> IO State
 
 -- TODO: Implement a random shuffling algorithm
+
 shuffleDeck state = return (state)
